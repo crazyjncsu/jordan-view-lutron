7b43044d552d349367fb665caa3cf0af128ba572
jake <jmorgan@screenconnect.com>
2021-12-05 18:07:46 -0500

    added damper stuff (but didn't run fixup/commit)

--- fixup.ps1
+++ fixup.ps1
@@ -60,8 +60,31 @@ Start-Job -RunAs32 -ArgumentList $PSScriptRoot\JordanView.hdf -ScriptBlock {
 
 
 
+	$connection.Execute("UPDATE EngravingText SET [Text] = 'Indoor Fireplace Damper' WHERE Parent_ID = 38 AND Position = 15") | Out-Null
+	$connection.Execute("UPDATE EngravingText SET [Text] = '' WHERE Parent_ID = 38 AND Position = 16") | Out-Null
+	$connection.Execute("UPDATE EngravingText SET [Text] = '' WHERE Parent_ID = 38 AND Position = 17") | Out-Null
+	$connection.Execute("UPDATE EngravingText SET [Text] = '' WHERE Parent_ID = 38 AND Position = 18") | Out-Null
+	$connection.Execute("UPDATE EngravingText SET [Text] = '' WHERE Parent_ID = 38 AND Position = 19") | Out-Null
+	$connection.Execute("UPDATE EngravingText SET [Text] = '' WHERE Parent_ID = 38 AND Position = 20") | Out-Null
+	$connection.Execute("UPDATE EngravingText SET [Text] = '' WHERE Parent_ID = 38 AND Position = 21") | Out-Null
+
+	$connection.Execute("UPDATE EngravingText SET [Text] = '' WHERE Parent_ID = 38 AND Position = 8") | Out-Null
+	$connection.Execute("UPDATE EngravingText SET [Text] = '' WHERE Parent_ID = 38 AND Position = 9") | Out-Null
+	$connection.Execute("UPDATE EngravingText SET [Text] = '' WHERE Parent_ID = 38 AND Position = 10") | Out-Null
+	$connection.Execute("UPDATE EngravingText SET [Text] = '' WHERE Parent_ID = 38 AND Position = 11") | Out-Null
+	$connection.Execute("UPDATE EngravingText SET [Text] = '' WHERE Parent_ID = 38 AND Position = 12") | Out-Null
+	$connection.Execute("UPDATE EngravingText SET [Text] = '' WHERE Parent_ID = 38 AND Position = 13") | Out-Null
+	$connection.Execute("UPDATE EngravingText SET [Text] = '' WHERE Parent_ID = 38 AND Position = 14") | Out-Null
+
+	$connection.Execute("UPDATE EngravingText SET [Text] = '' WHERE Parent_ID = 38 AND Position = 1") | Out-Null
+	$connection.Execute("UPDATE EngravingText SET [Text] = '' WHERE Parent_ID = 38 AND Position = 2") | Out-Null
+	$connection.Execute("UPDATE EngravingText SET [Text] = '' WHERE Parent_ID = 38 AND Position = 3") | Out-Null
+	$connection.Execute("UPDATE EngravingText SET [Text] = '' WHERE Parent_ID = 38 AND Position = 4") | Out-Null
+	$connection.Execute("UPDATE EngravingText SET [Text] = '' WHERE Parent_ID = 38 AND Position = 5") | Out-Null
+	$connection.Execute("UPDATE EngravingText SET [Text] = '' WHERE Parent_ID = 38 AND Position = 6") | Out-Null
+	$connection.Execute("UPDATE EngravingText SET [Text] = '' WHERE Parent_ID = 38 AND Position = 7") | Out-Null
+
+
+
 	$connection.Close()
 } | Wait-Job | Receive-Job
-
-git commit -a -m $commitMessage
-git push
